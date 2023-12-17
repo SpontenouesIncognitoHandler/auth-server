16d7832587ca04f4cd2bf370a8e956982bc39567
iamananya <ananyaiit123@gmail.com>
2023-12-17 17:52:20 +0530

    jwt implementation

--- authtoken/views.py
+++ authtoken/views.py
@@ -1,11 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .models import User
 from .serializers import UserSerializer
 
 class ListUser(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated] 
 
 class DetailUser(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
